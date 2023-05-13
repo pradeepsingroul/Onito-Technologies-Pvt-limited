57141ae208c8f5599125da977e1e6aff140cf119
predeep kumar singroul <prasingroul4899@gmail.com>
2023-05-13 17:43:41 +0530

    Ratings Exceptions

---
+++ OnitosProject/src/main/java/com/onito/Exceptions/RatingExceptions.java
@@ -0,0 +1,12 @@
+package com.onito.Exceptions;
+
+public class RatingExceptions extends RuntimeException{
+	
+	public RatingExceptions() {
+		
+	}
+	public RatingExceptions(String message) {
+		super(message);
+	}
+
+}
