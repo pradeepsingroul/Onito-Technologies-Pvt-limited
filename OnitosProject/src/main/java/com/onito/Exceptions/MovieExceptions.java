c46395020aede1868e5d0ca5def7b8a4e8422b06
predeep kumar singroul <prasingroul4899@gmail.com>
2023-05-13 17:43:05 +0530

    Movie Exceptions

---
+++ OnitosProject/src/main/java/com/onito/Exceptions/MovieExceptions.java
@@ -0,0 +1,16 @@
+package com.onito.Exceptions;
+
+
+
+public class MovieExceptions extends RuntimeException{
+	
+	public MovieExceptions() {
+		
+	}
+	public MovieExceptions(String message) {
+		super(message);
+	}
+	
+	
+
+}
