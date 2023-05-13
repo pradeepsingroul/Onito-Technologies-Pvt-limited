bce116ab1dd5671cf1ce3f73f7ab8c62e597eefb
predeep kumar singroul <prasingroul4899@gmail.com>
2023-05-13 17:37:43 +0530

    starting

---
+++ OnitosProject/src/main/java/com/onito/OnitosProjectApplication.java
@@ -0,0 +1,13 @@
+package com.onito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnitosProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnitosProjectApplication.class, args);
+	}
+
+}
